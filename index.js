0fb9f013f8536fdf5192a613727f2879ae0abaa3
Annika Kriskovich <annika.krisko2013@gmail.com>
2022-12-23 11:37:27 -0600

    commit tree 64df686f66558fb27d04a16bbeb16b4eb622c5ca

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+
+function writeCards(names, event) {
+    const thankYouMessages = [];
+    for (let i = 0; i < names.length; i++) {
+        thankYouMessages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return thankYouMessages;
+}
+
+const messages = writeCards(["Guadalupe", "Oli", "Aki"], "surprise");
+console.log(messages);
+
+
+
+function countDown (start) {
+    while (start >= 0) {
+        console.log(start);
+        start--;
+    }
+}
